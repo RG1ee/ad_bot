11961f968f73aba59b8bc77b9325588df283edf3
AlexeyGrushev <yablokov2004@mail.ru>
2023-01-30 20:36:44 +0300

    Added a funс to check if the user is subscribed

--- src/handlers/user.py
+++ src/handlers/user.py
@@ -1,8 +1,22 @@
 from aiogram import types, Dispatcher
+from settings.const import chat_id, chat_link
 
 
 async def start_message(message: types.Message):
-    await message.answer(f"Здравствуйте, {message.from_user.username}")
+
+    await message.answer(f"Здравствуйте, {message.from_user.first_name}")
+
+    sub_status = await message.bot.get_chat_member(
+        chat_id=chat_id,
+        user_id=message.chat.id
+        )
+    if sub_status.status != types.ChatMemberStatus.LEFT:
+        pass
+    else:
+        await message.answer(
+            "Для начала работы "
+            f"требуется подписка на телеграм канал:\n{chat_link}"
+            )
 
 
 def register_start_message(dp: Dispatcher):
